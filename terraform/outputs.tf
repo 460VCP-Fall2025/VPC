7cc050320c7adfd2d8a0ab22045029b0fbc499c0
Dustin <dcmarsh4@gmail.com>
2025-11-20 15:54:07 -0600

    added scp output

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -11,3 +11,8 @@ output "vpn_ec2_ssh_command" {
   description = "SSH command for VPN-EC2"
   value       = "ssh -i vpn-keypair.pem ubuntu@${aws_instance.vpn_ec2.public_ip}"
 }
+
+output "vpn_ec2_scp_command" {
+  description = "scp command for VPN-EC2"
+  value       = "scp -o StrictHostKeyChecking=no -i ./vpn-keypair.pem ubuntu@${aws_instance.vpn_ec2.public_ip}:/home/ubuntu/vpn-key.ovpn ${path.module}/vpn-key.ovpn"
+}
