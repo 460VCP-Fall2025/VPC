b98f30eac8c2030346f8c7acc87719c7b1fa4fbf
Maanyare28 <Maanyare28@gmail.com>
2025-11-21 15:20:48 -0600

    nat-script file

--- packer/nat/nat-script.sh
+++ packer/nat/nat-script.sh
@@ -1,8 +1,21 @@
- "sudo apt-get update -y",
-     "sudo apt-get install -y iptables-persistent net-tools",
-      "sudo sysctl -w net.ipv4.ip_forward=1",
-      "echo 'net.ipv4.ip_forward=1' | sudo tee -a /etc/sysctl.conf",
-      "sudo iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE",
-      "sudo iptables-save | sudo tee /etc/iptables/rules.v4",
-      "sudo systemctl enable netfilter-persistent",
-      "echo'NAT On'"
+#!/bin/bash      
+sleep 30
+
+"sudo apt update -y",
+"sudo apt install -y iptables ufw net-tools",
+
+"echo 1 | sudo tee /proc/sys/net/ipv4/ip_forward",
+"sudo sed -i 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/' /etc/sysctl.conf",
+"sudo sysctl -p",
+
+
+"sudo iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE",
+"sudo iptables -A FORWARD -i eth0 -o eth0 -m state --state RELATED,ESTABLISHED -j ACCEPT",
+"sudo iptables -A FORWARD -i eth0 -o eth0 -j ACCEPT",
+
+
+"echo '#!/bin/bash' | sudo tee /etc/rc.local",
+"echo 'iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE' | sudo tee -a /etc/rc.local",
+"echo 'exit 0' | sudo tee -a /etc/rc.local",
+"sudo chmod +x /etc/rc.local",
+"sudo systemctl daemon-reload"
--- packer/nat/nat.pkr.hcl
+++ packer/nat/nat.pkr.hcl
@@ -44,30 +44,10 @@ build {
   name    = "nat-build"
   sources = ["source.amazon-ebs.nat"]
 
-  provisioner "shell" {
-    inline = [
-      "sudo apt update -y",
-      "sudo apt install -y iptables ufw net-tools",
 
-      # Enable IP forwarding
-      "echo 1 | sudo tee /proc/sys/net/ipv4/ip_forward",
-      "sudo sed -i 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/' /etc/sysctl.conf",
-      "sudo sysctl -p",
 
-      # Set NAT rules
-      "sudo iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE",
-      "sudo iptables -A FORWARD -i eth0 -o eth0 -m state --state RELATED,ESTABLISHED -j ACCEPT",
-      "sudo iptables -A FORWARD -i eth0 -o eth0 -j ACCEPT",
-
-      # Enable rc.local support (for persistence)
-      "echo '#!/bin/bash' | sudo tee /etc/rc.local",
-      "echo 'iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE' | sudo tee -a /etc/rc.local",
-      "echo 'exit 0' | sudo tee -a /etc/rc.local",
-      "sudo chmod +x /etc/rc.local",
-      "sudo systemctl daemon-reload"
-    ]
+ provisioner "shell" {
+    script = "./nat-script.sh"
   }
-
-
 }
 
